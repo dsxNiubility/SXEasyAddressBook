16637ea402c5356d50e7a1953fad6b68fe48a3f1
dsx <dantesx2012@gmail.com>
2016-05-23 17:22:19 +0800

    iOS7的情况也设置了

--- SXEasyAddressBookDemo/SXEasyAddressBookDemo/ViewController3.m
+++ SXEasyAddressBookDemo/SXEasyAddressBookDemo/ViewController3.m
@@ -8,8 +8,9 @@
 
 // 这个类用来做iOS7的操作
 #import "ViewController3.h"
+#import <AddressBookUI/AddressBookUI.h>
 
-@interface ViewController3 ()
+@interface ViewController3 ()<ABPeoplePickerNavigationControllerDelegate>
 
 @end
 
@@ -17,22 +18,124 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self checkStatus];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored"-Wdeprecated-declarations"
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    // 1.创建选择联系人的控制器
+    ABPeoplePickerNavigationController *ppnc = [[ABPeoplePickerNavigationController alloc] init];
+    
+    ppnc.peoplePickerDelegate = self;
+    
+    [self presentViewController:ppnc animated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - <ABPeoplePickerNavigationControllerDelegate>
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)checkStatus
+{
+    // 1.获取授权状态
+    ABAuthorizationStatus status = ABAddressBookGetAuthorizationStatus();
+    
+    ABAddressBookRef addressBookRef = ABAddressBookCreateWithOptions(NULL, NULL);
+    
+    if (status == kABAuthorizationStatusNotDetermined) {
+        NSLog(@"还没问呢");
+        ABAddressBookRequestAccessWithCompletion(addressBookRef, ^(bool granted, CFErrorRef error){
+            if(granted){
+                NSLog(@"选择了同意");
+            }else{
+                NSLog(@"拒绝了");
+            }
+        });
+    }else if (status == kABAuthorizationStatusAuthorized){
+        NSLog(@"已经授权");
+        [self loadPerson];
+    }else {
+        NSLog(@"没有授权");
+        // 弹窗提示去获取权限
+    }
+    
+    
 }
-*/
+
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person
+{
+    return YES;
+}
+
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier
+{
+    ABMultiValueRef phone = ABRecordCopyValue(person, kABPersonPhoneProperty);
+    long index = ABMultiValueGetIndexForIdentifier(phone,identifier);
+    NSString *phoneNO = (__bridge NSString *)ABMultiValueCopyValueAtIndex(phone, index);
+    
+    CFStringRef lastName = ABRecordCopyValue(person, kABPersonLastNameProperty);
+    CFStringRef firstName = ABRecordCopyValue(person, kABPersonFirstNameProperty);
+    
+    NSString *lastname = (__bridge_transfer NSString *)(lastName);
+    NSString *firstname = (__bridge_transfer NSString *)(firstName);
+    
+    NSLog(@"%@ %@", lastname, firstname);
+    
+//    if ([phoneNO hasPrefix:@"+"]) {
+//        phoneNO = [phoneNO substringFromIndex:3];
+//    }
+//    phoneNO = [phoneNO stringByReplacingOccurrencesOfString:@"-" withString:@""];
+    
+    NSLog(@"%@", phoneNO);
+    if (phone) {
+        [peoplePicker dismissViewControllerAnimated:YES completion:nil];
+        return NO;
+    }
+    return YES;
+}
+
+- (void)loadPerson
+{
+    // 3.创建通信录对象
+    ABAddressBookRef addressBook = ABAddressBookCreate();
+    
+    // 4.获取所有的联系人
+    CFArrayRef peopleArray = ABAddressBookCopyArrayOfAllPeople(addressBook);
+    CFIndex peopleCount = CFArrayGetCount(peopleArray);
+    
+    // 5.遍历所有的联系人
+    for (int i = 0; i < peopleCount; i++) {
+        // 5.1.获取某一个联系人
+        ABRecordRef person = CFArrayGetValueAtIndex(peopleArray, i);
+        // 5.2.获取联系人的姓名
+        NSString *lastName = (__bridge_transfer NSString *)ABRecordCopyValue(person, kABPersonLastNameProperty);
+        NSString *firstName = (__bridge_transfer NSString *)ABRecordCopyValue(person, kABPersonFirstNameProperty);
+        NSLog(@"%@ %@", lastName, firstName);
+        
+        // 5.3.获取电话号码
+        // 5.3.1.获取所有的电话号码
+        ABMultiValueRef phones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+        CFIndex phoneCount = ABMultiValueGetCount(phones);
+        
+        // 5.3.2.遍历拿到每一个电话号码
+        for (int i = 0; i < phoneCount; i++) {
+            // 1.获取电话对应的key
+            NSString *phoneLabel = (__bridge_transfer NSString *)ABMultiValueCopyLabelAtIndex(phones, i);
+            
+            // 2.获取电话号码
+            NSString *phoneValue = (__bridge_transfer NSString *)ABMultiValueCopyValueAtIndex(phones, i);
+            
+            NSLog(@"%@ %@", phoneLabel, phoneValue);
+        }
+        
+        CFRelease(phones);
+    }
+    
+    CFRelease(addressBook);
+    CFRelease(peopleArray);
+}
+
+#pragma clang diagnostic pop
 
 @end
