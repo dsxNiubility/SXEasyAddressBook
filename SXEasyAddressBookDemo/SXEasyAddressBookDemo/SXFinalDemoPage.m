d5738f6597ed4d630be03014cc572457fb9d2791
dsx <dantesx2012@gmail.com>
2016-07-28 11:36:03 +0800

    加了判断 防止重复插入信息

--- SXEasyAddressBookDemo/SXEasyAddressBookDemo/SXFinalDemoPage.m
+++ SXEasyAddressBookDemo/SXEasyAddressBookDemo/SXFinalDemoPage.m
@@ -30,13 +30,17 @@
 {
     SXAddressBookAuthStatus status = [[SXAddressBookManager manager]getAuthStatus];
     if (status == kSXAddressBookAuthStatusNotDetermined) {
+        
         [[SXAddressBookManager manager]askUserWithSuccess:^{
             self.personEntityArray = [[SXAddressBookManager manager]getPersonInfoArray];
         } failure:^{
             NSLog(@"失败");
         }];
+        
     }else if (status == kSXAddressBookAuthStatusAuthorized){
+        
         self.personEntityArray = [[SXAddressBookManager manager]getPersonInfoArray];
+        
     }else{
         NSLog(@"没有权限");
     }
@@ -46,11 +50,17 @@
 - (void)checkStatus2
 {
     [[SXAddressBookManager manager]checkStatusAndDoSomethingSuccess:^{
+        
         NSLog(@"已经有权限，做相关操作，可以做读取通讯录等操作");
+        if(![[NSUserDefaults standardUserDefaults]boolForKey:@"inserted"]){
         [[SXAddressBookManager manager]creatItemWithName:@"雷克萨斯-北京咨询电话" phone:@"010-88657869"];
+            [[NSUserDefaults standardUserDefaults]setBool:YES forKey:@"inserted"];}
         self.personEntityArray = [[SXAddressBookManager manager]getPersonInfoArray];
+        
     } failure:^{
+        
         NSLog(@"未得到权限，做相关操作，可以做弹窗询问等操作");
+        
     }];
 }
 
